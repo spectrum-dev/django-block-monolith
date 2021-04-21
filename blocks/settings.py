65a3f77a8f2ef0abf6eac0129493ba5cad1c8c3b
Rahul V Brahmal <rahul.brahmal@me.com>
2021-04-21 11:11:50 +0100

    (feature): Created an app for signal_blocks.

--- blocks/settings.py
+++ blocks/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'computational_blocks',
+    'signal_blocks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
