8d584025c60fed630469a5707e439515f714cfa8
Rahul V Brahmal <rahul.brahmal@me.com>
2021-04-22 11:48:29 +0100

    (feature): Created a strategy_blocks app.

--- blocks/settings.py
+++ blocks/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'computational_blocks',
     'signal_blocks',
+    'strategy_blocks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
