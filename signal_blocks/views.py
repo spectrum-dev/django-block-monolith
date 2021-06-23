ebf11dce91f6057b0350ea94e7ee4515ad732394
Rahul V Brahmal <rahul.brahmal@me.com>
2021-06-23 21:00:40 +0100

    (feature): Added additional tests.

--- signal_blocks/views.py
+++ signal_blocks/views.py
@@ -4,6 +4,7 @@ import enum
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import serializers
+from rest_framework.views import APIView
 from rest_enumfield import EnumField
 
 from signal_blocks.blocks.event_block.main import run
@@ -32,24 +33,29 @@ def get_event_actions(request):
     return JsonResponse(response)
 
 
-def post_run(request):
-    """
-    Runs the event block
-    """
-    class EventType(enum.Enum):
-        INTERSECT = "INTERSECT"
-    
-    class EventAction(enum.Enum):
-        BUY = "BUY"
-        SELL = "SELL"
-    class InputSerializer(serializers.Serializer):
-        event_type = EnumField(choices=EventType)
-        event_action = EnumField(choices=EventAction)
+class PostRun(APIView):
+    def post(self, request):
+        """
+        Runs the event block
+        """
+        class EventType(enum.Enum):
+            INTERSECT = "INTERSECT"
+        
+        class EventAction(enum.Enum):
+            BUY = "BUY"
+            SELL = "SELL"
+        class InputSerializer(serializers.Serializer):
+            event_type = EnumField(choices=EventType)
+            event_action = EnumField(choices=EventAction)
 
-    request_body = json.loads(request.body)
+        request_body = json.loads(request.body)
 
-    response = []
-    if InputSerializer(data=request["input"]).is_valid(raise_exception=True):
+        response = []
+        InputSerializer(data=request_body["input"]).is_valid(raise_exception=True)
+        
+        if (len(request_body["output"].keys()) < 2):
+            raise serializers.ValidationError("You must pass in at least two different streams of data")
+        
         response = run(request_body["input"], request_body["output"])
 
-    return JsonResponse({"response": response})
+        return JsonResponse({"response": response})
