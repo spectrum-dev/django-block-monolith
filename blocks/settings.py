8c1658d0e9099687319188bdbc7c5cf9fd5d8e3a
Rahul V Brahmal <rahul.brahmal@me.com>
2021-08-02 15:38:12 +0100

    (fix): Maskes the secrets and debug.

--- blocks/settings.py
+++ blocks/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from os import environ
 from dotenv import load_dotenv
 
 from pathlib import Path
@@ -24,10 +25,10 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*m1olm61*qm^&ger^&*_b7fzx!0*(lum0qztn(8^905&j#y%qs"
+SECRET_KEY = environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = environ['DJANGO_DEBUG'] == "TRUE"
 
 # CORS
 CORS_ORIGIN_ALLOW_ALL = True
