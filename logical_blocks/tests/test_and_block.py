312ca3c0f6e75f9f5abc8f2970346231cf7fbeac
Rahul V Brahmal <rahul.brahmal@me.com>
2021-08-03 17:57:22 +0100

    (feature): Added a validation check if less than two streams are passed in.

--- logical_blocks/tests/test_and_block.py
+++ logical_blocks/tests/test_and_block.py
@@ -4,6 +4,23 @@ import json
 from django.test.testcases import TestCase
 
 class TestAndRun(TestCase):
+    def test_only_one_param_passed_in(self):
+        payload = {
+            "input": {},
+            "output": {
+                "SIGNAL_BLOCK-1-1": [{"timestamp": "01/02/2020", "order": "BUY"}, {"timestamp": "01/07/2020", "order": "BUY"}, {"timestamp": "01/32/2020", "order": "BUY"}],
+            },
+        }
+        
+        response = self.client.post(
+            "/LOGICAL_BLOCK/1/run",
+            json.dumps(payload),
+            content_type="application/json",
+        )
+
+        # TODO: Determine expected response
+        self.assertDictEqual(response.json(), {"non_field_errors": ["You must pass in at least two streams of data"]})
+
     def test_simple_two_param_and(self):
         payload = {
             "input": {},
@@ -45,7 +62,7 @@ class TestAndRun(TestCase):
             {'response': [{'timestamp': '01/07/2020', 'order': 'BUY'}]}
         )
 
-    def test_buy_sell_same_timestamp(self):
+    def test_buy_sell_same_timestamp_does_not_trigger_intersect(self):
         payload = {
             "input": {},
             "output": {
--- logical_blocks/views.py
+++ logical_blocks/views.py
@@ -15,6 +15,12 @@ class AndRunView(APIView):
     def post(self, request):
         request_body = json.loads(request.body)
 
+        if len(request_body["output"].keys()) < 2:
+            return JsonResponse(
+                {"non_field_errors": ["You must pass in at least two streams of data"]},
+                status=400,
+            )
+
         response = main(request_body['output'])
 
         return JsonResponse({"response": response})
