287e18dc5ff62cfe4c230064b463b1f7a50fcca1
Rahul V Brahmal <rahul.brahmal@me.com>
2021-08-03 17:10:11 +0100

    (feature): Created skeleton structure for AND block.

--- logical_blocks/views.py
+++ logical_blocks/views.py
@@ -1,3 +1,12 @@
+import enum
+
 from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_enumfield import EnumField
 
 # Create your views here.
+
+class AndRunView(APIView):
+    def post(self, request):
+        pass
