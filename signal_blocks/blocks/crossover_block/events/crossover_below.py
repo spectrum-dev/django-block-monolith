8f1726ec3b4c9d4bd553fa6a4df3c63fc8150f5f
Oscar Chang <oscarchang127@gmail.com>
2021-08-07 12:24:50 +0100

    added logic to compute whether action should be taken at each timestamp

---
+++ signal_blocks/blocks/crossover_block/events/crossover_below.py
@@ -0,0 +1,16 @@
+def main(df, action, crossover_value):
+    df = df.dropna()
+    df["order"] = None
+
+    i = 1
+    while i < df.shape[0]:
+        prev_value = df["data"][i - 1]
+        current_value = df["data"][i]
+        """
+        If current indicator value is below crossover point and is not a consecutive crossover, then place action
+        """
+        if current_value < crossover_value and prev_value >= crossover_value:
+            df["order"][i] = action
+        i += 1
+
+    return df
