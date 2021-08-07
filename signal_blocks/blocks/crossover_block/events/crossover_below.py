7f1a63dbff5633207a0ffd514d788a7a36be954e
Oscar Chang <oscarchang127@gmail.com>
2021-08-07 20:54:18 +0100

    lint

--- signal_blocks/blocks/crossover_block/events/crossover_below.py
+++ signal_blocks/blocks/crossover_block/events/crossover_below.py
@@ -2,6 +2,8 @@ def main(df, action, crossover_value):
     df = df.dropna()
     df["order"] = None
     # If current indicator value is below crossover point and is not a consecutive crossover, then place action
-    df.loc[(df.data < crossover_value) & (df.data.shift(1) >= crossover_value), "order"] = action
+    df.loc[
+        (df.data < crossover_value) & (df.data.shift(1) >= crossover_value), "order"
+    ] = action
 
     return df
