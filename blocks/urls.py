c0321dac27e97619acf566d9cf5d9560ec70649e
Oscar Chang <oscarchang127@gmail.com>
2021-08-09 20:24:30 +0100

    added or block skeleton

--- blocks/urls.py
+++ blocks/urls.py
@@ -46,5 +46,7 @@ urlpatterns = [
     path("SIGNAL_BLOCK/2/saddleType", signal_blocks.views.get_saddle_types),
     path("SIGNAL_BLOCK/2/run", signal_blocks.views.PostSaddleRun.as_view()),
     path("SIGNAL_BLOCK/3/run", signal_blocks.views.PostAndRunView.as_view()),
+    path("SIGNAL_BLOCK/5/eventAction", signal_blocks.views.get_event_actions),
+    path("SIGNAL_BLOCK/5/run", signal_blocks.views.PostOrRunView.as_view()),
     path("STRATEGY_BLOCK/1/run", strategy_blocks.views.PostRun.as_view()),
 ]
--- signal_blocks/views.py
+++ signal_blocks/views.py
@@ -10,6 +10,7 @@ from rest_enumfield import EnumField
 from signal_blocks.blocks.event_block.main import run as signal_block_run
 from signal_blocks.blocks.saddle_block.main import run as saddle_block_run
 from signal_blocks.blocks.and_block.main import run as and_run
+from signal_blocks.blocks.or_block.main import run as or_run
 
 
 # Create your views here.
@@ -134,5 +135,33 @@ class PostAndRunView(APIView):
             )
 
         response = and_run(request_body['output'])
+
+        return JsonResponse({"response": response})
+
+
+class PostOrRunView(APIView):
+    """
+    Runs an OR logic on output of 2 or more Signal Blocks
+    """
+    def post(self, request):
+        class EventAction(enum.Enum):
+            BUY = "BUY"
+            SELL = "SELL"
+        
+        class InputSerializer(serializers.Serializer):
+            event_action = EnumField(choices=EventAction)
+
+        request_body = json.loads(request.body)
+
+        response = []
+        InputSerializer(data=request_body["input"]).is_valid(raise_exception=True)
+
+        if len(request_body["output"].keys()) < 2:
+            return JsonResponse(
+                {"non_field_errors": ["You must pass in at least two streams of data"]},
+                status=400,
+            )
+
+        response = or_run(request_body['input'], request_body['output'])
 
         return JsonResponse({"response": response})
