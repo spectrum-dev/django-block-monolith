91c330c66ad92c11dbec9ad547d4da703f70bfb5
Oscar Chang <oscarchang127@gmail.com>
2021-08-12 16:38:52 +0100

    rearranged files for aesthetics

--- signal_blocks/tests/test_candle_close_block.py
+++ signal_blocks/tests/test_candle_close_block.py
@@ -1,8 +1,7 @@
 import json
 from django.test import TestCase
 
-from computational_blocks.blocks.technical_analysis.main import run
-from signal_blocks.tests.data.data_block import DATA_BLOCK
+from signal_blocks.tests.data.data_block import DATA_BLOCK, DATA_BLOCK_2
 
 
 class GetEventAction(TestCase):
@@ -258,53 +257,7 @@ class PostRun(TestCase):
                 "event_type": "CLOSE_ABOVE_OPEN",
             },
             "output": {
-                "DATA_BLOCK-1-1": [
-                    {
-                        "timestamp": "01/01/2020",
-                        "timezone": "UTC/EST",
-                        "open": "20.00",
-                        "high": "30.00",
-                        "low": "8.00",
-                        "close": "10.00",
-                        "volume": "10.00",
-                    },
-                    {
-                        "timestamp": "01/02/2020",
-                        "timezone": "UTC/EST",
-                        "open": "17.00",
-                        "high": "17.00",
-                        "low": "9.00",
-                        "close": "15.00",
-                        "volume": "100.00",
-                    },
-                    {
-                        "timestamp": "01/03/2020",
-                        "timezone": "UTC/EST",
-                        "open": "12.00",
-                        "high": "20.00",
-                        "low": "12.00",
-                        "close": "12.00",
-                        "volume": "12.00",
-                    },
-                    {
-                        "timestamp": "01/04/2020",
-                        "timezone": "UTC/EST",
-                        "open": "13.00",
-                        "high": "15.00",
-                        "low": "9.00",
-                        "close": "11.00",
-                        "volume": "13.00",
-                    },
-                    {
-                        "timestamp": "01/05/2020",
-                        "timezone": "UTC/EST",
-                        "open": "10.00",
-                        "high": "14.00",
-                        "low": "5.00",
-                        "close": "4.95",
-                        "volume": "14.00",
-                    },
-                ],
+                "DATA_BLOCK-1-1": DATA_BLOCK_2,
             },
         }
 
