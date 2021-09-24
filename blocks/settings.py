e61dc86624914e5bb07069cf25e7a7d2ae25866a
Rahul V Brahmal <rahul.brahmal@me.com>
2021-09-24 15:43:09 +0100

    (lint): Linted files.

--- blocks/settings.py
+++ blocks/settings.py
@@ -29,15 +29,13 @@ sentry_sdk.init(
     dsn=environ["SENTRY_DSN"],
     integrations=[DjangoIntegration()],
     environment="production",
-
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
     traces_sample_rate=1.0,
-
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
+    send_default_pii=True,
 )
 
 # Quick-start development settings - unsuitable for production
