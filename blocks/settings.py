99d72d96d40b306f65ffd455849b521453e8f046
Rahul V Brahmal <rahul.brahmal@me.com>
2021-10-26 23:02:59 +0100

    (fix): Fixed static file generation issue.

--- blocks/settings.py
+++ blocks/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import logging
 import sentry_sdk
 
-from os import environ
+from os import environ, path
 from dotenv import load_dotenv
 
 from pathlib import Path
@@ -175,6 +175,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,10 @@ python manage.py migrate
 echo "Apply 'data_bank' database migrations"
 python manage.py migrate data_store --database=data_bank
 
+# Collects Static Folders
+echo "Apply database migrations"
+python manage.py collectstatic
+
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
