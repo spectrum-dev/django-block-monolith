ab6621d962f033ca34158570f3af7f43dbe0f02a
Rahul V Brahmal <rahul.brahmal@me.com>
2021-10-30 19:15:20 +0100

    (fix): Ran autoflake to remove unused variables.

--- data_blocks/views.py
+++ data_blocks/views.py
@@ -1,17 +1,9 @@
-import json
-import enum
-
-from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework import serializers
-from rest_framework.views import APIView
-from rest_enumfield import EnumField
 
 
 # US Stock Data (Data Block with ID 1)
 # -----------------------------------
 from data_blocks.one.alpha_vantage import search_ticker
-from data_blocks.one.main import run as us_stock_data_run
 
 # Dropdowns
 def get_equity_name(request):
@@ -44,7 +36,6 @@ def get_crypto_candlesticks(request):
 # Crypto Data (Data Block with ID 2)
 # -----------------------------------
 from data_blocks.two.supported_crypto import SUPPORTED_CRYPTO
-from data_blocks.two.main import run as crypto_run
 
 
 def get_symbol(request):
