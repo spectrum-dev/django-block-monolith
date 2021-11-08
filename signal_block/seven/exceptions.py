56ff88292dce1ac1ba5cc54ea25b83a9808b7b09
Oscar Chang <oscarchang127@gmail.com>
2021-11-08 15:13:42 +0000

    added type hinting and validation for block

---
+++ signal_block/seven/exceptions.py
@@ -0,0 +1,26 @@
+class SignalBlockSevenInvalidInputPayloadException(Exception):
+    pass
+
+
+class SignalBlockSevenInvalidComparisonTypeException(Exception):
+    pass
+
+
+class SignalBlockSevenMissingInputException(Exception):
+    pass
+
+
+class SignalBlockSevenInputBlockOneMissingDataFieldException(Exception):
+    pass
+
+
+class SignalBlockSevenInputBlockTwoMissingDataFieldException(Exception):
+    pass
+
+
+class SignalBlockSevenMissingInputBlockOneException(Exception):
+    pass
+
+
+class SignalBlockSevenMissingInputBlockTwoException(Exception):
+    pass
